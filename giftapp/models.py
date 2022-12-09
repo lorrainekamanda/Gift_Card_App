47348713fb26aac58784b023031f888c804c30ab
Lorraine Kamanda <lorrainekamanda@gmail.com>
2022-12-10 01:28:54 +0300

    Add Abstract Base User Manager to allow user to register only using email,password

--- giftapp/models.py
+++ giftapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 
 
