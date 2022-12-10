7914f836156907ddc706638fdfa625af3ee3721e
Lorraine Kamanda <lorrainekamanda@gmail.com>
2022-12-10 13:03:58 +0300

    serialize Custom User

---
+++ giftapp/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+
+        fields = [
+
+            'uniqueID','email', 'password'
+        ]
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+
+        return instance
--- giftprojects/settings.py
+++ giftprojects/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'giftapp',
-    'djmoney'
+    'djmoney',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
