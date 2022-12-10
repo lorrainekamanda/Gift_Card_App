54ee320817c990591ff27221a2030d102e4e4d82
Lorraine Kamanda <lorrainekamanda@gmail.com>
2022-12-11 02:47:32 +0300

    add Products Category Crud

--- giftapp/urls.py
+++ giftapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView,ProductsView,ProductView
+from .views import RegisterView, LoginView,ProductsView,ProductView,ProductCategoryView,ProductsCategoryView
 
 urlpatterns = [
     path('sign-up/', RegisterView.as_view(), name='sign_up'),
     path('sign-in/', LoginView.as_view(), name='sign_in'),
     path('products-view/', ProductsView.as_view(), name='products_view'),
     path('product/<pk>/', ProductView.as_view(), name='product_detail'),
+    path('category-view/', ProductsCategoryView.as_view(), name='category_view'),
+    path('category/<pk>/', ProductCategoryView.as_view(), name='category_detail'),
 ]
--- giftapp/views.py
+++ giftapp/views.py
@@ -66,9 +66,6 @@ def is_authenticated(request, *args, **kwargs):
     return True
 
 
-
-
-
 class ProductsView(mixins.ListModelMixin, mixins.CreateModelMixin,
                 generics.GenericAPIView):
 
@@ -110,6 +107,51 @@ class ProductView(mixins.RetrieveModelMixin,
 
       
         return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        if not is_authenticated(request):
+            raise AuthenticationFailed('Unauthenticated')
+
+        obj_id = kwargs.pop('pk')
+        obj = self.queryset.filter(id=obj_id).first()
+
+       
+
+        return self.destroy(request, *args, **kwargs)
+
+
+
+class ProductsCategoryView(mixins.ListModelMixin, mixins.CreateModelMixin,
+                generics.GenericAPIView):
+
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+    def get(self, request, *args, **kwargs):
+       
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if not is_authenticated(request):
+            raise AuthenticationFailed('Unauthenticated')
+
+        return self.create(request, *args, **kwargs)
+
+
+
+class ProductCategoryView(mixins.RetrieveModelMixin,
+               mixins.UpdateModelMixin,
+               mixins.DestroyModelMixin,
+               generics.GenericAPIView):
+
+    """ get and delete a productCategory """
+
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        
+        return self.retrieve(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         if not is_authenticated(request):
